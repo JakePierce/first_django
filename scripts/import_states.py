4cd2448aafe16cad1084fa73091008d14c2816fc
Jacob Pierce <Jake@Jacobs-MacBook-Pro.local>
2015-12-09 15:08:29 -0700

    import script changes

--- scripts/import_states.py
+++ scripts/import_states.py
@@ -26,27 +26,27 @@ csv_file = open(states_csv, 'r')
 
 reader = csv.DictReader(csv_file)
 
-# for row in reader:
-#     new_state, created = State.objects.get_or_create(name=row['state'])
-#     new_state.abbrev = row['abbrev']
-#     new_state.save()
-
-#     new_capital, created = StateCapital.objects.get_or_create(name=row['capital'])
-#     new_capital.lat = row['latitude']
-#     new_capital.lon = row['longitude']
-#     new_capital.pop = row['population']
-
-#     new_capital.state = new_state
-#     new_capital.save()
-
 for row in reader:
-    new_area, created = City.objects.get_or_create(name=row['city'])
-    new_area.zip_code = row['zip_code']
-    new_area.lat = row['latitude']
-    new_area.lon = row['longitude']
-    new_area.city = row['city']
-    new_area.abbrev = row['state']
-    new_area.county = row['county']
+    new_state, created = State.objects.get_or_create(name=row['state'])
+    new_state.abbrev = row['abbrev']
+    new_state.save()
+
+    new_capital, created = StateCapital.objects.get_or_create(name=row['capital'])
+    new_capital.lat = row['latitude']
+    new_capital.lon = row['longitude']
+    new_capital.pop = row['population']
+
+    new_capital.state = new_state
+    new_capital.save()
+
+# for row in reader:
+#     new_area, created = City.objects.get_or_create(name=row['city'])
+#     new_area.zip_code = row['zip_code']
+#     new_area.lat = row['latitude']
+#     new_area.lon = row['longitude']
+#     new_area.city = row['city']
+#     new_area.abbrev = row['state']
+#     new_area.county = row['county']
 
 
     # state = State.objects.get()
